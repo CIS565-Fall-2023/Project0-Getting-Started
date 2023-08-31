fb452bf0ecc84b1b8f96ae734db243a2f128adb2
cao <licheng_cao@126.com>
2023-08-31 10:49:24 -0400

    3.1.1

--- cuda-getting-started/src/main.cpp
+++ cuda-getting-started/src/main.cpp
@@ -11,7 +11,7 @@
  */
 int main(int argc, char* argv[]) {
     // TODO: Change this line to use your name!
-    m_yourName = "TODO: YOUR NAME HERE";
+    m_yourName = "Licheng Cao";
 
     if (init(argc, argv)) {
         mainLoop();
