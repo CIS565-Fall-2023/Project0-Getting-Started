04ccd5e6a481c30946e8a2d9727ad5d23abc359e
TongHuoAo <tonghu218@gmail.com>
2023-09-01 17:42:28 -0400

    Add readme.

--- dxr-support/src/D3D12RaytracingHelloWorld/Raytracing.hlsl
+++ dxr-support/src/D3D12RaytracingHelloWorld/Raytracing.hlsl
@@ -69,7 +69,8 @@ void MyRaygenShader()
 [shader("closesthit")]
 void MyClosestHitShader(inout RayPayload payload, in MyAttributes attr)
 {
-    float3 barycentrics = float3(1 - attr.barycentrics.x - attr.barycentrics.y, attr.barycentrics.x, attr.barycentrics.y);
+    //float3 barycentrics = float3(1 - attr.barycentrics.x - attr.barycentrics.y, attr.barycentrics.x, attr.barycentrics.y);
+    float3 barycentrics = float3(0.2, 0.8, 0.5);
     payload.color = float4(barycentrics, 1);
 }
 
