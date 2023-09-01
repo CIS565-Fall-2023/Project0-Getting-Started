9fafd200bd811e47f79acb6abd4e4f26d70d48ba
GrahamZen <931252924@qq.com>
2023-09-01 17:56:23 +0800

    modified the name

--- cuda-getting-started/src/main.cpp
+++ cuda-getting-started/src/main.cpp
@@ -10,8 +10,8 @@
  * C main function.
  */
 int main(int argc, char* argv[]) {
-    // TODO: Change this line to use your name!
-    m_yourName = "TODO: YOUR NAME HERE";
+    // DONE: Change this line to use your name!
+    m_yourName = "Gehan Zheng";
 
     if (init(argc, argv)) {
         mainLoop();
